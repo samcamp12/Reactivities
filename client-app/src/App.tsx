11e02f952837771f88ed9e32b8f611e7b545356c
samcamp12 <jiwang24@gwu.edu>
2023-01-24 23:46:28 +0800

    added frontend

---
+++ client-app/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import axios from 'axios';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+
+function App() {
+
+  const [activities, setActivities] = React.useState([]);
+  
+  React.useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      <Header as="h2" icon="users" content="Reactivities"/>
+      <List>
+        {activities.map<JSX.Element>((activity: any) => {
+          return (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>)
+        })}
+      </List>
+    </div>
+  );
+}
+
+export default App;
